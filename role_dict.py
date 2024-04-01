b1e58e1c6a196b425307265a8350df686ad4c86d
Anton Franssen <AntonFranssen@yandex.ru>
2024-04-01 22:49:47 +0300

    Нерабочая хуета

--- role_dict.py
+++
@@ -1,8 +0,0 @@
-# что-то мне подсказывает, что ниже написанное - полный пиздец, но я пока ниче лучше не придумал
-from bot import *
-
-role_dict = {
-    "admin": discord.Permissions.elevated(),
-    "moderator": discord.Permissions.stage_moderator(),
-    "member": discord.Permissions.general()
-}
